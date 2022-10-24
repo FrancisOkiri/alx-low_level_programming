530a0363fe18d569547439e7d54d4022d5128f1c
root <francisokiri33@gmail.com>
2022-10-24 12:13:07 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return:On success l else -l 
+ */
+int _putchar(char c)
+{
+	return (write(l, &c, l));
+}	
+
